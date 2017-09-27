30a045302f78ba0b153873f7a6a95c065f058fa5
Parv Kumar <parvathikumar92@gmail.com>
2017-09-27 22:52:48 +0000

    automatically backed up by learn

--- db/migrate/20170927224312_add_post_tags_to_post_tag.rb
+++ db/migrate/20170927224312_add_post_tags_to_post_tag.rb
@@ -1,6 +1,6 @@
-class AddPostTagsToPostTag <ActiveRecord::Migration 
-  def change 
-    add_column :post_tag, :post, :belongs_to 
-    add_column :post_tag, :tag, :belongs_to 
-  end 
+class AddPostTagsToPostTag <ActiveRecord::Migration
+  def change
+    add_column :post_tag, :post, :belongs_to
+    add_column :post_tag, :tag, :belongs_to
+  end
 end
