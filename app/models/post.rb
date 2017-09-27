ed2a7959be0488f6b40c242f4b5725357b64b9fb
Parv Kumar <parvathikumar92@gmail.com>
2017-09-27 22:36:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :content, presence: true
   belongs_to :user 
+  has_many :post_tags
+  has_many :posts, through: :post_tags
+
+  
 end
