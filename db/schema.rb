ca734c8dfc68d54d9f0189aa3ecea9599363a117
Parv Kumar <parvathikumar92@gmail.com>
2017-09-27 22:53:17 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927224055) do
+ActiveRecord::Schema.define(version: 20170927224312) do
 
-  create_table "post_tags", force: :cascade do |t|
-    t.integer  "post_id"
-    t.integer  "tag_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "post_tags" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
